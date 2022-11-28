b1e2bbfa43df3526311ba0ec4caeab80868ae1fa
Dikachi-official <nwokontadikachi@gmail.com>
2022-11-28 23:07:01 +0100

    Integrating Filter to API

--- api/views.py
+++ api/views.py
@@ -5,11 +5,15 @@ from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from .filter import ProductFilter
 
 # Create your views here.
 class ProductViewSet(ModelViewSet):
     queryset= Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
 
 class CategoryViewSet(ModelViewSet):
